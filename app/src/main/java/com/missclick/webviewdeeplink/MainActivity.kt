038c01be454c57efaec0379d439c78ddbdb90809
Nikita <neketapostolov@gmail.com>
2020-10-02 13:50:32 +0300

    fb

--- app/src/main/java/com/missclick/webviewdeeplink/MainActivity.kt
+++ app/src/main/java/com/missclick/webviewdeeplink/MainActivity.kt
@@ -2,10 +2,12 @@ package com.missclick.webviewdeeplink
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.missclick.webviewdeeplink.data.FbHelp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val fb = FbHelp(this).tree()
     }
 }
---
+++ app/src/main/java/com/missclick/webviewdeeplink/data/FbHelp.kt
@@ -0,0 +1,25 @@
+package com.missclick.webviewdeeplink.data
+
+import android.app.Activity
+import android.content.Context
+import android.net.Uri
+import android.util.Log
+import androidx.browser.customtabs.CustomTabsIntent
+import com.facebook.applinks.AppLinkData
+
+class FbHelp(val context: Context) {
+
+     fun tree() {
+        AppLinkData.fetchDeferredAppLinkData(context
+        ) { appLinkData: AppLinkData? ->
+            if (appLinkData != null && appLinkData.targetUri != null) {
+                if (appLinkData.argumentBundle["target_url"] != null) {
+                    //Msg().messageSchedule(context)
+                    Log.e("DEEP", "SRABOTAL")
+                    val tree = appLinkData.argumentBundle["target_url"].toString()
+
+                }
+            }
+        }
+    }
+}
